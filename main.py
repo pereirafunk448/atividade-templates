4cb192905bc22203887440e2693cdbf47efdcfed
pereirafunk448 <87548413+pereirafunk448@users.noreply.github.com>
2022-03-18 00:36:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask('app')
+
+@app.route('/')
+def template():
+  return render_template('index.html')
+
+@app.route('/unifran')
+def unifran():
+  return render_template('uni.html')
+  
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
